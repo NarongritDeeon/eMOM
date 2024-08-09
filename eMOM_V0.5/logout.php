39b69fa2347cc4038e68fce2e9086911c64deb63
NarongritDeeon <35131312+NarongritDeeon@users.noreply.github.com>
2024-08-09 12:06:39 +0700

    Delete eMOM_V0.5 directory

--- eMOM_V0.5/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Clear all session variables
-session_destroy(); // Destroy the session
-
-header('Location: index.php'); // Redirect to the login page or any other page
-exit();
-
-?>
